136edce0fc61961884bb4d20d739aa43cacb47dc
bodeyprovansal <bprovansal@gmail.com>
2020-06-30 18:57:26 +0000

    pc-pi-pip starter comm code

--- sps_pi.js
+++ sps_pi.js
@@ -7,15 +7,17 @@ const port = new SerialPort('/dev/ttyUSB0', () => {
 	console.log('Connection Made to device at: ' + port.path)
 })
 
-const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
+const parser = port.pipe(new Readline({ delimiter: '\n' }))
 parser.on('data', console.log)
 
 const server = net.createServer(function(socket) {
   console.log('server: tcp server started')
 
   socket.on('data', function(data) {
+    //Here 'data' is received from client
     console.log('server: ' + data + ' from ' + socket.remoteAddress + ':'
 	          + socket.remotePort)
+    //Here 'data' is being sent back to client
     socket.write('server repeating: ' + data)
   })
 
@@ -23,12 +25,17 @@ const server = net.createServer(function(socket) {
     console.log('server: client closed connection')
   })
   
-  socket.write('Echo server\r\n')
+  //socket.write('Echo server\r\n')
+  //Client Receives info back from port/device
   socket.pipe(socket)
+  //Info from device back to server
   socket.pipe(port)
+  parser.on('data', function(data) {
+    socket.write(data)
+  })
 })
 
-server.listen(1337, '127.0.0.1')
+server.listen(1337, '10.10.1.179')
 
 //port.pipe(parser)
 //parser.on('data', console.log)
