874b17a9b6fecae534b13831971a628b23de2e54
bodeyprovansal <bprovansal@gmail.com>
2020-07-01 15:53:35 -0700

    update pi server

--- sps_pi.js
+++ sps_pi.js
@@ -2,12 +2,17 @@ const net = require('net')
 const SerialPort = require('serialport')
 const Readline = require('@serialport/parser-readline')
 
+const HOST = '127.0.1.1'
+const PORTNUM = 1337
+
 const port = new SerialPort('/dev/ttyUSB0', () => {
 	baudRate: 9600
 	console.log('Connection Made to device at: ' + port.path)
 })
 
-const parser = port.pipe(new Readline({ delimiter: '\n' }))
+port.on('open', console.log)
+
+const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
 parser.on('data', console.log)
 
 const server = net.createServer(function(socket) {
@@ -19,6 +24,9 @@ const server = net.createServer(function(socket) {
 	          + socket.remotePort)
     //Here 'data' is being sent back to client
     socket.write('server repeating: ' + data)
+    //port.write('\r')
+    port.write(data.toString())
+    port.write('\r')
   })
 
   socket.on('close', function() {
@@ -27,17 +35,18 @@ const server = net.createServer(function(socket) {
   
   //socket.write('Echo server\r\n')
   //Client Receives info back from port/device
-  socket.pipe(socket)
+  //socket.pipe(socket)
   //Info from device back to server
-  socket.pipe(port)
+  ///socket.pipe(port)
   parser.on('data', function(data) {
+    console.log('dataBackToClient: ')
     socket.write(data)
   })
 })
 
-server.listen(1337, '10.10.1.179')
+server.listen(PORTNUM, '192.168.1.188')
 
 //port.pipe(parser)
 //parser.on('data', console.log)
-port.write('\r')
+//port.write('\r')
 
